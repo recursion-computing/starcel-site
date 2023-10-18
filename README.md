054e936546efed8b9b3375e572ab917654d63b23
Nick Maleki <xnick99@gmail.com>
2023-10-18 01:54:43 -0400

    Added Stars, email capture, and info pages

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Starcel landing page
 
 Sources:
 https://codepen.io/Juxtopposed/pen/MWZWpVQ inspired by effect on https://zajno.com/
+https://github.com/kaizhelam/stars-animation-effect
---
+++ main.js
@@ -0,0 +1,262 @@
+import * as THREE from 'three';
+
+// Stars Background
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d'),
+    w = canvas.width = window.innerWidth,
+    h = canvas.height = window.innerHeight,
+
+    hue = 0,
+    stars = [],
+    count = 0,
+    maxStars = 1400;
+
+var canvas2 = document.createElement('canvas'),
+    ctx2 = canvas2.getContext('2d');
+canvas2.width = 100;
+canvas2.height = 100;
+var half = canvas2.width / 2,
+    gradient2 = ctx2.createRadialGradient(half, half, 0, half, half, half);
+gradient2.addColorStop(0.025, '#fff');
+gradient2.addColorStop(0.07, 'hsl(' + hue + ', 0%, 33%)');
+gradient2.addColorStop(0.15, 'hsl(' + hue + ', 0%, 6%)');
+gradient2.addColorStop(1, 'transparent');
+
+ctx2.fillStyle = gradient2;
+ctx2.beginPath();
+ctx2.arc(half, half, half, 0, Math.PI * 2);
+ctx2.fill();
+
+// End cache
+
+function random(min, max) {
+    if (arguments.length < 2) {
+        max = min;
+        min = 0;
+    }
+
+    if (min > max) {
+        var hold = max;
+        max = min;
+        min = hold;
+    }
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function maxOrbit(x, y) {
+    var max = Math.max(x, y),
+        diameter = Math.round(Math.sqrt(max * max + max * max));
+    return diameter / 2;
+}
+
+var Star = function () {
+    this.orbitRadius = random(maxOrbit(w, h));
+    this.radius = random(60, this.orbitRadius) / 12;
+    this.orbitX = w / 2;
+    this.orbitY = h / 2;
+    this.timePassed = random(0, maxStars);
+    this.speed = -1 * random(this.orbitRadius) / 2500000;
+    this.alpha = random(2, 10) / 10;
+
+    count++;
+    stars[count] = this;
+}
+
+Star.prototype.draw = function () {
+    var x = Math.sin(this.timePassed) * this.orbitRadius + this.orbitX,
+        y = Math.cos(this.timePassed) * this.orbitRadius + this.orbitY,
+        twinkle = random(100);
+
+    if (twinkle === 1 && this.alpha > 0) {
+        this.alpha -= 0.05;
+    } else if (twinkle === 2 && this.alpha < 1) {
+        this.alpha += 0.05;
+    }
+
+    ctx.globalAlpha = this.alpha;
+    ctx.drawImage(canvas2, x - this.radius / 2, y - this.radius / 2, this.radius, this.radius);
+    this.timePassed += this.speed;
+}
+
+for (var i = 0; i < maxStars; i++) {
+    new Star();
+}
+
+function animation() {
+    ctx.globalCompositeOperation = 'source-over';
+    ctx.globalAlpha = 0.8;
+    ctx.fillStyle = 'hsla(' + hue + ', 0%, 6%, 1)';
+    w = canvas.width = window.innerWidth
+    h = canvas.height = window.innerHeight
+    ctx.fillRect(0, 0, w, h)
+
+    ctx.globalCompositeOperation = 'lighter';
+    for (var i = 1, l = stars.length; i < l; i++) {
+        stars[i].draw();
+    };
+
+    window.requestAnimationFrame(animation);
+}
+
+animation();
+
+
+// Glitch Effect applied to the onload of the image 
+const imageContainer = document.getElementById("imageContainer");
+const imageElement = document.getElementById("myImage");
+
+//imageElement.onload = function(){
+let easeFactor = 0.01;
+let scene, camera, renderer, planeMesh;
+let mousePosition = { x: 0.5, y: 0.5 };
+let targetMousePosition = { x: 0.5, y: 0.5 };
+let mouseStopTimeout;
+let aberrationIntensity = 1;
+let lastPosition = { x: 0.5, y: 0.5 };
+let prevPosition = { x: 0.5, y: 0.5 };
+
+// shaders
+const vertexShader = `
+varying vec2 vUv;
+void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
+`;
+
+const fragmentShader = `
+varying vec2 vUv;
+uniform sampler2D u_texture;    
+uniform vec2 u_mouse;
+uniform vec2 u_prevMouse;
+uniform float u_aberrationIntensity;
+
+void main() {
+    vec2 gridUV = floor(vUv * vec2(11.0, 11.0)) / vec2(11.0, 11.0);
+    vec2 centerOfPixel = gridUV + vec2(1.0/11.0, 1.0/11.0);
+    
+    vec2 mouseDirection = u_mouse - u_prevMouse;
+    
+    vec2 pixelToMouseDirection = centerOfPixel - u_mouse;
+    float pixelDistanceToMouse = length(pixelToMouseDirection);
+    float strength = smoothstep(0.3, 0.0, pixelDistanceToMouse);
+
+    vec2 uvOffset = strength * - mouseDirection * 0.2;
+    vec2 uv = vUv - uvOffset;
+
+    vec4 colorR = texture2D(u_texture, uv + vec2(strength * u_aberrationIntensity * 0.01, 0.0));
+    vec4 colorG = texture2D(u_texture, uv);
+    vec4 colorB = texture2D(u_texture, uv - vec2(strength * u_aberrationIntensity * 0.01, 0.0));
+    vec4 colorA = texture2D(u_texture, uv);
+
+    gl_FragColor = vec4(colorR.r, colorG.g, colorB.b, colorA.a);
+}
+`;
+
+function initializeScene(texture) {
+    // scene creation
+    scene = new THREE.Scene();
+
+    // camera setup
+    camera = new THREE.PerspectiveCamera(
+        80,
+        imageElement.offsetWidth / imageElement.offsetHeight,
+        0.01,
+        10
+    );
+    camera.position.z = 1;
+
+    // uniforms
+    let shaderUniforms = {
+        u_mouse: { type: "v2", value: new THREE.Vector2() },
+        u_prevMouse: { type: "v2", value: new THREE.Vector2() },
+        u_aberrationIntensity: { type: "f", value: 0.0 },
+        u_texture: { type: "t", value: texture }
+    };
+
+    // creating a plane mesh with materials
+    planeMesh = new THREE.Mesh(
+        
+        new THREE.PlaneGeometry(2, 2),
+        new THREE.ShaderMaterial({
+            uniforms: shaderUniforms,
+            vertexShader,
+            fragmentShader,
+            transparent: true, //appears to remove the abberation coloring
+        })
+    );
+    scene.background=null;
+    // add mesh to scene
+    scene.add(planeMesh);
+    
+
+    // render
+    renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setClearColor( 0x000000, 0 );
+    renderer.setSize(imageElement.offsetWidth, imageElement.offsetHeight);
+    //camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 2000);
+
+    // create a canvas
+    imageContainer.appendChild(renderer.domElement);
+}
+
+// use the existing image from html in the canvas
+initializeScene(new THREE.TextureLoader().load(imageElement.src));
+
+animateScene();
+
+function animateScene() {
+    requestAnimationFrame(animateScene);
+
+    mousePosition.x += (targetMousePosition.x - mousePosition.x) * easeFactor;
+    mousePosition.y += (targetMousePosition.y - mousePosition.y) * easeFactor;
+
+    planeMesh.material.uniforms.u_mouse.value.set(
+        mousePosition.x,
+        1.0 - mousePosition.y
+    );
+
+    planeMesh.material.uniforms.u_prevMouse.value.set(
+        prevPosition.x,
+        1.0 - prevPosition.y
+    );
+
+    aberrationIntensity = Math.max(0.0, aberrationIntensity - 0.05);
+
+    planeMesh.material.uniforms.u_aberrationIntensity.value = aberrationIntensity;
+
+    renderer.render(scene, camera);
+
+}
+
+// event listeners
+imageContainer.addEventListener("mousemove", handleMouseMove);
+imageContainer.addEventListener("mouseenter", handleMouseEnter);
+imageContainer.addEventListener("mouseleave", handleMouseLeave);
+
+function handleMouseMove(event) {
+    easeFactor = 0.02;
+    let rect = imageContainer.getBoundingClientRect();
+    prevPosition = { ...targetMousePosition };
+
+    targetMousePosition.x = (event.clientX - rect.left) / rect.width;
+    targetMousePosition.y = (event.clientY - rect.top) / rect.height;
+
+    aberrationIntensity = 1;
+
+}
+
+function handleMouseEnter(event) {
+    easeFactor = 0.02;
+    let rect = imageContainer.getBoundingClientRect();
+
+    mousePosition.x = targetMousePosition.x = (event.clientX - rect.left) / rect.width;
+    mousePosition.y = targetMousePosition.y = (event.clientY - rect.top) / rect.height;
+}
+
+function handleMouseLeave() {
+    easeFactor = 0.05;
+    targetMousePosition = { ...prevPosition };
+}
+//};
